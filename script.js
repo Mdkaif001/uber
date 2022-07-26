60443511ed01c65345fa111bd515b96affd81a12
Mdkaif001 <mkkaif002@gmail.com>
2022-07-26 10:04:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let scroll =[
+    {
+        id:"1"
+    },
+    {
+        id:"2"
+    },
+
+    {
+        id:"3"
+    },
+    {
+        id:"4"
+    },
+    {
+        id:"5"
+    },
+    {
+        id:"6"
+    },
+]
+let left_scroll = document.getElementById("left_scroll");
+let right_scroll = document.getElementById("right_scroll");
+let mainListing = document.getElementsByClassName("mainListing")[0];
+// let mainListing = Array.from(document.getElementsByClassName("mainListing"))[0];
+
+// left_scroll.addEventListener("click",()=>{
+//     mainListing.scrollLeft -=330;
+// });
+
+// function left() {
+//     console.log(index);
+//     mainListing.scrollLeft -=330;
+// }
+// function right() {
+//     mainListing.scrollLeft -=330;
+}
+// right_scroll.addEventListener("click",()=>{
+//     mainListing.scrollLeft +=330;
+// });
